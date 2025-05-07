abd65b467ed9e553090156d74439bc306c171952
Rerigan <cosnnum@gmail.com>
2025-05-07 09:52:45 -0300

    first steps

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <header className="block justify-center text-center mt-20">
+        <h1 className="font-bold text-7xl text-center block">Typ_</h1>
+        <h2 className="text-xl text-center block mt-6">How fast can you type?</h2>
+    </header>
+  )
+}
+
+export default Header
